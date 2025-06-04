05a9a8ca9dab1401accd7af3c965c8bf0a7269db
Ngọc Anh <69442009+TruongNgocAnh23@users.noreply.github.com>
2025-06-04 19:50:51 +0700

    new code

---
+++ identity-service/src/controllers/identity-controller.js
@@ -0,0 +1,52 @@
+const logger = require('../utils/logger');
+const { validateRegistration } = require('../utils/validation')
+const User = require('../models/User');
+const generateTokens = require('../utils/generateToken');
+// user registration
+const registerUser = async (req, res) => {
+    logger.info('Resgistration end point hit...');
+    try {
+        const { error } = validateRegistration(req.body);
+        if (error) {
+            logger.warn('Validation error', error.details[0].message);
+            return res.status(400).json({
+                success: false,
+                message: error.details[0].message
+            })
+        }
+        const { email, username, password } = req.body;
+        let user = await User.findOne({ $or: [{ email }, { username }] });
+        if (user) {
+            logger.warn('User already exists');
+            return res.status(400).json({
+                success: false,
+                message: 'User already exists'
+            });
+        }
+        user = new User({ username, email, password });
+        await user.save();
+        logger.warn('User saved successfully', user._id);
+
+        const { accessToken, refreshToken } = generateTokens(user);
+        return res.status(201).json({
+            success: true,
+            message: 'User registered successfully',
+            accesstoken: accessToken,
+            refreshtoken: refreshToken
+        })
+    }
+    catch (err) {
+        logger.error('Registration error occured', err);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        });
+    }
+}
+// user login
+
+// refresh token
+
+//logout
+
+module.exports = { registerUser };
